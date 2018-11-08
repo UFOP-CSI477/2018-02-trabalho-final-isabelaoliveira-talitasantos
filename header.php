d90fa4a98dca4278186fd76bdf4fd9ede251101b
talita20 <talitasv20@gmail.com>
2018-11-08 21:48:24 -0200

    organização das pastas

---
+++ header.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8"/>
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png"/>
+    <link rel="icon" type="image/png" href="assets/img/favicon.png"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Outside Food</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+    <!--  Material Dashboard CSS    -->
+    <link href="assets/css/material-dashboard.css?v=1.2.0" rel="stylesheet"/>
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="assets/css/demo.css" rel="stylesheet"/>
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet'
+          type='text/css'>
+</head>
+
+<body>
+<div class="wrapper">
+    <div class="sidebar" data-color="purple" data-image="assets/img/sidebar-1.jpg">
+        <div class="logo">
+            <a href="./admin.php" class="simple-text">
+                <img class="logo1" src="assets/img/oft1.png">
+            </a>
+        </div>
+        <div class="sidebar-wrapper">
+            <ul class="nav">
+                <li id="dashboard">
+                    <a href="./admin.php">
+                        <i class="material-icons">dashboard</i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li id="eventos">
+                    <a href="">
+                        <i class="material-icons">event</i>
+                        <p>Evento</p>
+                    </a>
+                </li>
+                <li id="edicao">
+                    <a href="">
+                        <i class="material-icons">content_paste</i>
+                        <p>Edição</p>
+                    </a>
+                </li>
+                <li id="cidade">
+                    <a href="">
+                        <i class="material-icons">location_city</i>
+                        <p>Cidade</p>
+                    </a>
+                </li>
+                <li id="local">
+                    <a href="">
+                        <i class="material-icons">location_on</i>
+                        <p>Local</p>
+                    </a>
+                </li>
+                <li id="espaco">
+                    <a href="">
+                        <i class="material-icons">store</i>
+                        <p>Espaços</p>
+                    </a>
+                </li>
+                <li id="culinaria">
+                    <a href="">
+                        <i class="material-icons">photo_library</i>
+                        <p>Culinária</p>
+                    </a>
+                </li>
+                <li id="album">
+                    <a href="">
+                        <i class="material-icons">photo_library</i>
+                        <p>Álbum de Fotos</p>
+                    </a>
+                </li>
+                <li class="active-pro" style="padding-bottom: 50px;">
+
+                    <a href="./index.php">
+                        <i class="material-icons">exit_to_app</i>
+                        <p>Sair</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
